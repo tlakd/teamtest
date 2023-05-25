a07ad81367a08076a4ca0c593efc817d974b908e
zndntn66 <zndntn66@gmail.com>
2023-05-25 11:54:03 +0900

    실험

--- testteam/src/testteam/test.java
+++ testteam/src/testteam/test.java
@@ -1,9 +1,11 @@
 // 정욱진 1차 수정
 //윤성용
 // 김주환
-branch 'master' of https://github.com/tlakd/teamtest.git
 package testteam;
 
 public class test {
 // 바뀌나?
+	public static void main(String[] args) {
+		System.out.println("실험 2");
+	}
 }
