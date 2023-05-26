c0ad85afe1a8fe69bf1d1dd72fdf9a3656e6ecc8
zndntn66 <zndntn66@gmail.com>
2023-05-26 10:25:49 +0900

    makeroom 수정1

--- testteam/src/testteam/MakeRoom.java
+++ testteam/src/testteam/MakeRoom.java
@@ -7,8 +7,10 @@ import java.util.List;
 import java.util.Map;
 
 public class MakeRoom {
-	Map<Integer, List<NumberSave>> lottoNumber = new HashMap<>();
-	Map<Integer, List<NumberSave>> userNumber = new HashMap<>();
+	Map<Integer, List<NumberSave>> lottoNumber = new HashMap<>();// 로또 번호 저장
+	Map<Integer, List<NumberSave>> userNumber = new HashMap<>(); // 유저 번호 저장
+	Map<Integer, String> autoNotAuto = new HashMap<>(); // 자동 반자동 수동
+
 	int userCount = 1;
 	int lottoCount = 101;
 	int MaxPay = 100000;
@@ -27,17 +29,26 @@ public class MakeRoom {
 	}
 
 	public void makeAutoNumber(int a) {// 사용자의 로또번호 자동생성 초안
-		if (!userNumber.containsKey(userCount)) {
-			userNumber.put(userCount, new ArrayList<>());
-		}
-		while (userNumber.get(userCount).size() < 6) {
-			int q = (int) (Math.random() * 45) + 1;
-			NumberSave newNumber = new NumberSave(q);
-			if (!userNumber.get(userCount).contains(newNumber)) {
-				userNumber.get(userCount).add(newNumber);
+		for (int i = userCount; i < userCount + a; i++) {
+			int count = 0;
+			if (!userNumber.containsKey(i)) {
+				userNumber.put(i, new ArrayList<>());
+			}
+			while (userNumber.get(i).size() < 6) {
+				int q = (int) (Math.random() * 45) + 1;
+				NumberSave newNumber = new NumberSave(q);
+				if (!userNumber.get(i).contains(newNumber)) {
+					userNumber.get(i).add(newNumber);
+					count++;
+				}
+			}
+			userCount++;
+			if (count == 6) {
+				autoNotAuto.put(i, "자 동");
+			} else if (count < 6) {
+				autoNotAuto.put(i, "반자동");
 			}
 		}
-		userCount++;
 	}
 
 	public void makeLottoNumber() {// 이때 까지의 로또 1등번호 뽑기 초안 7자리 처음 만들때
@@ -46,11 +57,12 @@ public class MakeRoom {
 		}
 	}
 
-	public void makeLottoNumberNew() {// 다음 회차 될때 로또 1등번호 뽑기 초안(있는거 로또 다음 회차 생성, 샀던 개수 초기화 살수있는 값 초기화)
+	public void makeLottoNumberNew() {// 다음 회차 될때 로또 1등번호 뽑기 초안(로또 다음 회차 생성, 샀던 개수 초기화 살수있는 값 초기화, 이때 까지 유저가 샀던 기록 초기화)
 		lottoCount++;
 		makeMachineLotto(lottoCount);
 		userCount = 1;
 		MaxPay = 100000;
+		userNumber.clear();
 	}
 
 	public void lookList(int i) {// 유저 번호 순서대로 나열
@@ -66,5 +78,9 @@ public class MakeRoom {
 		} else {
 			// 결제 완료
 		}
+	}
+
+	public void 수동부분() {// 만들어야됨
+
 	}
 }
