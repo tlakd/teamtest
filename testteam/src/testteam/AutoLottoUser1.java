c2628b6a88e972491e8c0c16841f4c0cc4f896a5
jungwookjin <134483553+jungwookjin@users.noreply.github.com>
2023-06-01 17:35:38 +0900

    Update AutoLottoUser1.java
    GUI 부분 수정했습니다.

--- testteam/src/testteam/AutoLottoUser1.java
+++ testteam/src/testteam/AutoLottoUser1.java
@@ -5,17 +5,12 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.net.URL;
 
-import javax.swing.GroupLayout;
 import javax.swing.ImageIcon;
-import javax.swing.GroupLayout.Alignment;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JTextField;
-import javax.swing.LayoutStyle.ComponentPlacement;
 import java.awt.Font;
-import javax.swing.SwingConstants;
-import java.awt.Color;
 
 public class AutoLottoUser1 {
    private JFrame frame;
@@ -72,7 +67,7 @@ public class AutoLottoUser1 {
       btnNum1.setFont(new Font("굴림", Font.BOLD, 0));
       btnNum1.setIcon(new ImageIcon(AutoLottoUser1.class.getResource("/image/1번.jpg")));
       btnNum1.setSelectedIcon(new ImageIcon(AutoLottoUser1.class.getResource("/image/1번.jpg")));
-      btnNum1.setBounds(75, 182, 49, 31);
+      btnNum1.setBounds(242, 182, 49, 31);
       btnNum1.setOpaque(false);
       btnNum1.setBorderPainted(false);
 
@@ -80,7 +75,7 @@ public class AutoLottoUser1 {
       btnNum2.setFont(new Font("굴림", Font.BOLD, 0));
       btnNum2.setIcon(new ImageIcon(AutoLottoUser1.class.getResource("/image/2번.jpg")));
       btnNum2.setSelectedIcon(new ImageIcon(AutoLottoUser1.class.getResource("/image/2번.jpg")));
-      btnNum2.setBounds(145, 182, 49, 31);
+      btnNum2.setBounds(312, 182, 49, 31);
       btnNum2.setOpaque(false);
       btnNum2.setBorderPainted(false);
 
@@ -88,7 +83,7 @@ public class AutoLottoUser1 {
       btnNum3.setFont(new Font("굴림", Font.BOLD, 0));
       btnNum3.setIcon(new ImageIcon(AutoLottoUser1.class.getResource("/image/3번.jpg")));
       btnNum3.setSelectedIcon(new ImageIcon(AutoLottoUser1.class.getResource("/image/3번.jpg")));
-      btnNum3.setBounds(212, 182, 49, 31);
+      btnNum3.setBounds(379, 182, 49, 31);
       btnNum3.setOpaque(false);
       btnNum3.setBorderPainted(false);
 
@@ -96,7 +91,7 @@ public class AutoLottoUser1 {
       btnNum4.setFont(new Font("굴림", Font.BOLD, 0));
       btnNum4.setIcon(new ImageIcon(AutoLottoUser1.class.getResource("/image/4번.jpg")));
       btnNum4.setSelectedIcon(new ImageIcon(AutoLottoUser1.class.getResource("/image/4번.jpg")));
-      btnNum4.setBounds(75, 228, 49, 31);
+      btnNum4.setBounds(242, 228, 49, 31);
       btnNum4.setOpaque(false);
       btnNum4.setBorderPainted(false);
 
@@ -104,7 +99,7 @@ public class AutoLottoUser1 {
       btnNum5.setFont(new Font("굴림", Font.BOLD, 0));
       btnNum5.setIcon(new ImageIcon(AutoLottoUser1.class.getResource("/image/5번.jpg")));
       btnNum5.setSelectedIcon(new ImageIcon(AutoLottoUser1.class.getResource("/image/5번.jpg")));
-      btnNum5.setBounds(143, 228, 49, 31);
+      btnNum5.setBounds(310, 228, 49, 31);
       btnNum5.setOpaque(false);
       btnNum5.setBorderPainted(false);
 
@@ -112,7 +107,7 @@ public class AutoLottoUser1 {
       btnNum6.setFont(new Font("굴림", Font.BOLD, 0));
       btnNum6.setIcon(new ImageIcon(AutoLottoUser1.class.getResource("/image/6번.jpg")));
       btnNum6.setSelectedIcon(new ImageIcon(AutoLottoUser1.class.getResource("/image/6번.jpg")));
-      btnNum6.setBounds(212, 228, 49, 31);
+      btnNum6.setBounds(379, 228, 49, 31);
       btnNum6.setOpaque(false);
       btnNum6.setBorderPainted(false);
 
@@ -120,7 +115,7 @@ public class AutoLottoUser1 {
       btnNum7.setFont(new Font("굴림", Font.BOLD, 0));
       btnNum7.setIcon(new ImageIcon(AutoLottoUser1.class.getResource("/image/7번.jpg")));
       btnNum7.setSelectedIcon(new ImageIcon(AutoLottoUser1.class.getResource("/image/7번.jpg")));
-      btnNum7.setBounds(75, 276, 49, 31);
+      btnNum7.setBounds(242, 276, 49, 31);
       btnNum7.setOpaque(false);
       btnNum7.setBorderPainted(false);
 
@@ -128,7 +123,7 @@ public class AutoLottoUser1 {
       btnNum8.setFont(new Font("굴림", Font.BOLD, 0));
       btnNum8.setIcon(new ImageIcon(AutoLottoUser1.class.getResource("/image/8번.jpg")));
       btnNum8.setSelectedIcon(new ImageIcon(AutoLottoUser1.class.getResource("/image/8번.jpg")));
-      btnNum8.setBounds(143, 277, 49, 31);
+      btnNum8.setBounds(310, 277, 49, 31);
       btnNum8.setOpaque(false);
       btnNum8.setBorderPainted(false);
 
@@ -136,7 +131,7 @@ public class AutoLottoUser1 {
       btnNum9.setFont(new Font("굴림", Font.BOLD, 0));
       btnNum9.setIcon(new ImageIcon(AutoLottoUser1.class.getResource("/image/9번.jpg")));
       btnNum9.setSelectedIcon(new ImageIcon(AutoLottoUser1.class.getResource("/image/9번.jpg")));
-      btnNum9.setBounds(212, 276, 49, 31);
+      btnNum9.setBounds(379, 276, 49, 31);
       btnNum9.setOpaque(false);
       btnNum9.setBorderPainted(false);
 
@@ -144,7 +139,7 @@ public class AutoLottoUser1 {
       btnNum0.setFont(new Font("굴림", Font.PLAIN, 0));
       btnNum0.setIcon(new ImageIcon(AutoLottoUser1.class.getResource("/image/0번.jpg")));
       btnNum0.setSelectedIcon(new ImageIcon(AutoLottoUser1.class.getResource("/image/0번.jpg")));
-      btnNum0.setBounds(143, 325, 49, 31);
+      btnNum0.setBounds(310, 325, 49, 31);
       btnNum0.setOpaque(false);
       btnNum0.setBorderPainted(false);
       
@@ -152,42 +147,42 @@ public class AutoLottoUser1 {
       JButton btnNum1000 = new JButton("");
       btnNum1000.setIcon(new ImageIcon(AutoLottoUser1.class.getResource("/image/1000원.jpg")));
       btnNum1000.setSelectedIcon(new ImageIcon(AutoLottoUser1.class.getResource("/image/1000원.jpg")));
-      btnNum1000.setBounds(325, 180, 96, 32);
+      btnNum1000.setBounds(76, 180, 96, 32);
       btnNum1000.setOpaque(false);
       btnNum1000.setBorderPainted(false);
 
       JButton btnNum2000 = new JButton("");
       btnNum2000.setIcon(new ImageIcon(AutoLottoUser1.class.getResource("/image/2000원.jpg")));
       btnNum2000.setSelectedIcon(new ImageIcon(AutoLottoUser1.class.getResource("/image/2000원.jpg")));
-      btnNum2000.setBounds(325, 218, 96, 32);
+      btnNum2000.setBounds(76, 218, 96, 32);
       btnNum2000.setOpaque(false);
       btnNum2000.setBorderPainted(false);
 
       JButton btnNum3000 = new JButton("");
       btnNum3000.setIcon(new ImageIcon(AutoLottoUser1.class.getResource("/image/3000원.jpg")));
       btnNum3000.setSelectedIcon(new ImageIcon(AutoLottoUser1.class.getResource("/image/3000원.jpg")));
-      btnNum3000.setBounds(325, 253, 96, 32);
+      btnNum3000.setBounds(76, 253, 96, 32);
       btnNum3000.setOpaque(false);
       btnNum3000.setBorderPainted(false);
 
       JButton btnNum4000 = new JButton("");
       btnNum4000.setIcon(new ImageIcon(AutoLottoUser1.class.getResource("/image/4000원.jpg")));
       btnNum4000.setSelectedIcon(new ImageIcon(AutoLottoUser1.class.getResource("/image/4000원.jpg")));
-      btnNum4000.setBounds(325, 288, 96, 32);
+      btnNum4000.setBounds(76, 288, 96, 32);
       btnNum4000.setOpaque(false);
       btnNum4000.setBorderPainted(false);
 
       JButton btnNum5000 = new JButton("");
       btnNum5000.setIcon(new ImageIcon(AutoLottoUser1.class.getResource("/image/5000원.jpg")));
       btnNum5000.setSelectedIcon(new ImageIcon(AutoLottoUser1.class.getResource("/image/5000원.jpg")));
-      btnNum5000.setBounds(325, 324, 96, 32);
+      btnNum5000.setBounds(76, 324, 96, 32);
       btnNum5000.setOpaque(false);
       btnNum5000.setBorderPainted(false);
 
       JButton btnNo = new JButton("");
       btnNo.setIcon(new ImageIcon(AutoLottoUser1.class.getResource("/image/취소.jpg")));
       btnNo.setSelectedIcon(new ImageIcon(AutoLottoUser1.class.getResource("/image/취소.jpg")));
-      btnNo.setBounds(75, 325, 49, 31);
+      btnNo.setBounds(242, 325, 49, 31);
       btnNo.setOpaque(false);
       btnNo.setBorderPainted(false);
 
@@ -195,7 +190,7 @@ public class AutoLottoUser1 {
       btnOk.setIcon(new ImageIcon(AutoLottoUser1.class.getResource("/image/확인버튼.jpg")));
       btnOk.setSelectedIcon(new ImageIcon(AutoLottoUser1.class.getResource("/image/확인버튼.jpg")));
       btnOk.setFont(new Font("굴림", Font.PLAIN, 9));
-      btnOk.setBounds(212, 325, 49, 31);
+      btnOk.setBounds(379, 325, 49, 31);
       btnOk.setOpaque(false);
       btnOk.setBorderPainted(false);
 
@@ -346,7 +341,7 @@ public class AutoLottoUser1 {
       frame.getContentPane().add(btnNum1000);
       
       JLabel lblNewLabel_1 = new JLabel("New label");
-      lblNewLabel_1.setIcon(new ImageIcon(AutoLottoUser1.class.getResource("/image/금액계산기 수정2.jpg")));
+      lblNewLabel_1.setIcon(new ImageIcon(AutoLottoUser1.class.getResource("/image/금액계산기 수정3.jpg")));
       lblNewLabel_1.setBounds(0, 0, 496, 377);
       frame.getContentPane().add(lblNewLabel_1);
    }
@@ -373,9 +368,5 @@ public class AutoLottoUser1 {
          //makeRoom.exeAut();
          
       }
-      
    }
-   
-   
-   
 }
